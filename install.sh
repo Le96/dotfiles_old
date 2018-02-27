0aafd7cc46ecaf0f655ffc217a2536a260143d9c
Le96 <blitz1216@gmail.com>
2018-02-27 20:52:31 +0900

    add installation process for dein.toml

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 if [ ! -d ~/.config/nvim ]; then
   mkdir -p ~/.config/nvim
 fi
+ln -sf $(cd $(dirname $0) && pwd)/dein.toml ~/.config/nvim/dein.toml
 ln -sf $(cd $(dirname $0) && pwd)/init.vim ~/.config/nvim/init.vim
 ln -sf $(cd $(dirname $0) && pwd)/zshrc ~/.zshrc
