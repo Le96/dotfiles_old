395af790aaef89aa03886c6263f0551aaf51e79c
Le96 <blitz1216@gmail.com>
2018-02-27 20:43:53 +0900

    fix vimrc to zshrc bug

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ if [ ! -d ~/.config/nvim ]; then
   mkdir -p ~/.config/nvim
 fi
 ln -sf $(cd $(dirname $0) && pwd)/init.vim ~/.config/nvim/init.vim
-ln -sf $(cd $(dirname $0) && pwd)/vimrc ~/.vimrc
+ln -sf $(cd $(dirname $0) && pwd)/zshrc ~/.zshrc
